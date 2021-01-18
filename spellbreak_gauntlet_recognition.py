286be4fe018debc8b3827849b4addd2629f8956c
romarioop <48642776+RomarioOP@users.noreply.github.com>
2021-01-19 00:34:24 +0100

    Added gauntlet swap recognition and a couple of to-do's

--- spellbreak_gauntlet_recognition.py
+++ spellbreak_gauntlet_recognition.py
@@ -15,15 +15,17 @@ import threading
 import inspect
 import ctypes
 import sys
+import pathlib
+#https://stackoverflow.com/questions/27050492/how-do-you-create-a-tkinter-gui-stop-button-to-break-an-infinite-loop
+#https://stackoverflow.com/questions/3430372/how-do-i-get-the-full-path-of-the-current-files-directory
 
-
-#To-do: read user settings 
 #Store session data in json file
-#Ensure location to look for icons is always in folder where the script runs
 #Look for match cancel
+#change look for match string to LogLoad: LogLoad: LoadMap: 23.109.51.20:8570
+
+#Look for clash end of match
 #[2021.01.16-14.03.27:969][246]LogGameMode:Display: Match State Changed from WaitingToStart to LeavingMap !json{"pid":13380,"env":"production","ver":"2.0.10050"}
 #Create gui if needed
-
 
 #Get latest log file
 def find_latest_log_file():
@@ -63,9 +65,8 @@ def find_match_line_in_file(match, x):
     match_line_number=''.join(regex_match_line_number)
     match_line_number = match_line_number.replace(',', '')
     match_line_number = match_line_number.replace('(', '')
-
     match_info['Match'+x+'LineNumber'] = match_line_number
-    #print (match_info)
+ 
 
 #Find character class in string and assign the correct main hand element
 def find_main_hand(file_name):
@@ -92,8 +93,10 @@ def gauntlet():
         global last_offhand
         global last_used_gauntlet
         global main_hand
+        global working_dir
         for i in elements:
-            if pyautogui.locateOnScreen("H:\\Documents\\Programming\\Spellbreak\\Elements\\"+(i)+".png", region=(region), grayscale=True, confidence=0.8) != None:
+            if pyautogui.locateOnScreen((working_dir)+"\\Elements\\"+(i)+".png", region=(region), grayscale=True, confidence=0.8) != None:
+            #if pyautogui.locateOnScreen("H:\\Documents\\Programming\\Spellbreak\\Elements\\"+(i)+".png", region=(region), grayscale=True, confidence=0.8) != None:
                 if last_offhand != i or last_offhand != last_used_gauntlet:
                     print("Switched from "+(last_offhand)+" to "+(i))                
                     rgb[(i)]()
@@ -102,16 +105,8 @@ def gauntlet():
                 else:
                     print("Attacking with the same gauntlet as before. Skipping api call.")
 
-# def terminate():
-#     print("lets stop it")
-#     for thread in threading.enumerate(): 
-#         print(thread.name)
-
-    
-
 #Check if a match has been started and finished
 def find_completed_match():
-    #global should_restart
     global completed_match
     global t1
     global t2
@@ -140,39 +135,45 @@ def find_completed_match():
                 time.sleep(5)
                 start_complete_script()
                 return
-                
-                #should_restart=True
         else:
             time.sleep(3)
 
 #Track mouse events
 def check_mouse_input():
-    #global completed_match
     global last_used_gauntlet
     global main_hand
     while completed_match==False:
         for i in range(1, 256):
             if win32api.GetAsyncKeyState(i):
                 if i in special_keys:
-                    ### Main hand code
-                    if i == 1:
-                        print ("Attacking with main hand.")
-                        if last_used_gauntlet==main_hand:
-                            print("Light is already set to main hand settings. Skipping api call.")
-                        else:
-                            print("Activating color change.")
-                            rgb[(main_hand)]()
-                            last_used_gauntlet=main_hand
-                    ### End main hand code
-                    elif i == 2:
-                        print ("Attacking with off hand.")
-                        print ("Running gauntlet function.")
-                        gauntlet()
+                    if gauntlet_swap==False:
+                        if i == 1:
+                            print ("Attacking with main hand.")
+                            if last_used_gauntlet==main_hand:
+                                print("Light is already set to main hand settings. Skipping api call.")
+                            else:
+                                print("Activating color change.")
+                                rgb[(main_hand)]()
+                                last_used_gauntlet=main_hand
+                        elif i == 2:
+                            print ("Attacking with off hand.")
+                            print ("Running gauntlet function.")
+                            gauntlet()
+                    else:
+                        if i == 2:
+                            print ("Attacking with main hand.")
+                            if last_used_gauntlet==main_hand:
+                                print("Light is already set to main hand settings. Skipping api call.")
+                            else:
+                                print("Activating color change.")
+                                rgb[(main_hand)]()
+                                last_used_gauntlet=main_hand
+                        elif i == 1:
+                            print ("Attacking with off hand.")
+                            print ("Running gauntlet function.")
+                            gauntlet()                    
         time.sleep(0.3)
-    
         
-
-
 #Set rgb color codes
 def set_rgb_codes():
     global main_hand
@@ -195,7 +196,6 @@ def set_rgb_codes():
         elements.append(i)
     elements.remove(main_hand)
     
-   
 #Check if a matches have been found
 def find_matches():
     while match_found==False:
@@ -216,44 +216,39 @@ def find_matches():
                 print("Old match, restarting function")
                 time.sleep(1)
                 continue
-                #return match_found
             else:
                 print("this is a new match")
                 session_match=(latest_started_match)
                 print(session_match)
                 match_found==True
-                #time.sleep(5)
+                #Not sure if return below is required. Further testing needed
                 return session_match
-                #return match_found
-            #return match_found  
             continue 
         else: 
             print ("No Matches found yet.")
             time.sleep(1)
             continue
        
-
 #Assign the region/location of the screen that has to be looked over to find the off hand
 def set_gauntlet_position():
     global region
+    global gauntlet_swap
     with open('C:\\Users\\romar\\AppData\\Local\\g3\\Saved\\Config\\WindowsNoEditor\\GameUserSettings.ini') as f:
         if 'bSwapGauntletSlots=False' in f.read():
             region=(1150,915,65,70)
+            gauntlet_swap=False
         else:
-            region=("tbd")
+            region=(700,915,65,70)
+            gauntlet_swap=True
 
 #Set Special keys
-#To-do: get this info from ini file
-global special_keys
-special_keys = [0x01, 0x02]
-special = {0x01: 'leftClick',
-           0x02: 'rightClick',}
-time.sleep(1)
-
-
-
-#Start of script!
-#Define global variables.
+def set_special_keys():
+    global special_keys
+    special_keys = [0x01, 0x02]
+    special = {0x01: 'leftClick', 0x02: 'rightClick',}
+    time.sleep(1)
+   
+#Full code execution!
 def start_complete_script():
     global latest_file
     global match_found
@@ -263,10 +258,8 @@ def start_complete_script():
     global last_offhand
     global main_hand
     global session_match
-    #global should_restart
     global t1
     global t2
-    #should_restart=False
     latest_file=""
     match_found=False
     match_info={}
@@ -274,12 +267,10 @@ def start_complete_script():
     last_used_gauntlet=""
     last_offhand=""
     main_hand=""
-    #session_match=()
     #Find a started match in the latest log file. Function is a loop that resets the latest log file
     find_matches()
     print ("Matches found")
     #If matches found then find last match.
-    #latest_started_match=matched_start_lines[(len(matched_start_lines)-1)]
     print ("Latest match:", latest_started_match)
     #Find match start time and match start line number and add to match information list.
     find_match_times(latest_started_match, 'Start')
@@ -287,6 +278,7 @@ def start_complete_script():
     #Now that a match has been found, the mainhand can be determined.
     find_main_hand(latest_started_match)
     set_gauntlet_position()
+    set_special_keys()
     #Main hand found, setting rgb codes:
     set_rgb_codes()
     #Start checking for mouse input and start looking for end of match.
@@ -305,7 +297,7 @@ def start_complete_script():
     #             json.dump (match_info, fp)
     #         break
     
-
-
+global working_dir
+working_dir=str(pathlib.Path(__file__).parent.absolute())
 session_match=()
 start_complete_script()
